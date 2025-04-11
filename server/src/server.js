8d3fcf02bf0ee98a804d00141ca2726432e1f49b
Nusrath <nusrathjahan0204@gmail.com>
2025-04-11 20:49:22 +0200

    login function set

--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,31 @@ const app = express();
 
 
 app.use(morgan('dev'));
+
+const isLoggedIn = (req, res, next) => {
+ const login = false;
+ if(login){
+  next();
+ }else{
+  return res.status(401).json({ message: 'Please login first'});
+ }
+  next();
+};
+
+app.use(isLoggedIn);
+
 app.get("/test", (req, res) => {
     res.status(200).send({
         message: "Api is working"
       })
 });
+
+app.get("/api/user", isLoggedIn,(req, res) => {
+  res.status(200).send({
+      message: "User profile is returned"
+    })
+});
+
 
 app.listen(3001, () => {
   console.log("Server running at http://localhost:3001");
